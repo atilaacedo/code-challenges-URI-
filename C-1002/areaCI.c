401aa282e822ee23426e93151175afa6073dc4ba
atilaacedo <bolsonarolindaogostosao@gmail.com>
2020-08-05 22:06:36 -0300

    Mais exercícios

--- C-1002/areaCI.c
+++ C-1002/areaCI.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <math.h>   
+   
+ int main (){
+ 
+   double r, area;
+   scanf("%lf",&r);
+  
+   area = 3.14159 * r * r;
+   printf("A = %.4lf\n", area);
 
-int main(void){
 
-}
+  return 0;
+ }  
---
+++ C-1004/prod.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+  int a;
+  int b;
+
+  scanf("%d", &a);
+  scanf("%d", &b);
+
+  int PROD = a * b;
+
+  printf("PROD = %d\n", PROD);
+
+  return 0;
+}
